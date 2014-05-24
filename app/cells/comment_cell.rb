c7e5ab638047c58f9c69f325c111fc380ee1540f
amyilake <ilake.chang@gmail.com>
2014-05-24 16:08:18 +0800

    refactor

--- app/cells/comment_cell.rb
+++ app/cells/comment_cell.rb
@@ -1,7 +1,8 @@
 class CommentCell < Cell::Rails
 
   def show(args)
-    @post = args[:post] 
+    @post = args[:post]
+    @current_user = args[:current_user]
     @comments = @post.comments
     @comment = Comment.new(:commentable => @post)
     render
